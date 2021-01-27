469fac4c31941e71df2ca2389e406c4320a16187
Luke Finch <lukechristianfinch@gmail.com>
2021-01-27 09:27:09 +0000

    Move UI into own folder in src

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env, argv) => ({
 	devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
 	entry: {
-		ui: './src/ui.ts', // The entry point for your UI code
+		ui: './src/ui/ui.ts', // The entry point for your UI code
 		code: './src/code.ts' // The entry point for your plugin code
 	},
 
@@ -77,7 +77,7 @@ module.exports = (env, argv) => ({
 						after: { include: ['dist/ui.js'] }
 					}),
 					new HtmlWebpackPlugin({
-						template: './src/ui.html',
+						template: './src/ui/ui.html',
 						filename: 'ui.html',
 						inlineSource: '.(js|css|scss)$',
 						chunks: ['ui']
@@ -87,7 +87,7 @@ module.exports = (env, argv) => ({
 			: [
 					new VueLoaderPlugin(),
 					new HtmlWebpackPlugin({
-						template: './src/ui.html',
+						template: './src/ui/ui.html',
 						filename: 'ui.html',
 						inlineSource: '.(js|css|scss)$',
 						chunks: ['ui']
